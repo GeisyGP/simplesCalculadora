0357afab84e86c12b6d6f07dcce2b39077555eb8
Geisy Poncio <geisygabri@gmail.com>
2023-01-20 10:54:46 -0300

    Apresenta menu de escolha de operação.

--- main.c
+++ main.c
@@ -2,23 +2,100 @@
 #include <locale.h>
 #include <math.h>
 
-main (void){
-	setlocale(LC_ALL, "Portuguese");
-	float num1, num2, soma, subtracao, multiplicacao, divisao, potencia;
-	
-	printf ("Informe o primeiro e o segundo número: ");
+void soma(){
+	float num1, num2, resultado;
+
+	printf ("Informe os dois valores: ");
 	scanf ("%f", &num1);
 	scanf ("%f", &num2);
 	
-	soma = num1 + num2;
-	subtracao = num1 - num2;
-	multiplicacao = num1 * num2;
-	divisao = num1 / num2;
-	potencia = pow(num1, num2);
+	resultado = num1 + num2;
+	printf ("\nO resultado é %.2f", resultado);
+}
+
+void subtracao(){
+	float num1, num2, resultado;
 	
-	printf ("\nSoma: %.2f", soma);
-	printf ("\nSubtração: %.2f", subtracao);
-	printf ("\nMultiplicação: %.2f", multiplicacao);
-	printf ("\nDivisão: %.2f", divisao);
-	printf ("\nPotencia: %.2f", potencia);
+	printf ("Informe o minuendo: ");
+	scanf ("%f", &num1);
+	printf ("Informe o subtraendo: ");
+	scanf ("%f", &num2);			
+	
+	resultado = num1 - num2;
+	printf ("\nO resultado é %.2f", resultado);
+}
+
+void multiplicacao(){
+	float num1, num2, resultado;
+	
+	printf ("Informe os dois valores: ");
+	scanf ("%f", &num1);
+	scanf ("%f", &num2);			
+	
+	resultado = num1 * num2;	
+	printf ("\nO resultado é %.2f", resultado);
+}
+
+void divisao (){
+	float num1, num2, resultado;
+	
+	printf ("Informe o dividendo: ");
+	scanf ("%f", &num1);
+	printf ("Informe o divisor: ");
+	scanf ("%f", &num2);			
+	
+	resultado = num1 / num2;	
+	printf ("\nO resultado é %.2f", resultado);	
+}
+
+void potencia(){
+	int num1, num2, resultado;
+
+	printf ("Informe a base: ");
+	scanf ("%d", &num1);
+	printf ("Informe o expoente: ");
+	scanf ("%d", &num2);
+			
+	resultado = pow(num1, num2);
+	printf ("\nO resultado é %d", resultado);
+}
+
+int menu (int op){
+	printf ("******Menu******");
+	printf ("\n1 - Somar \n2 - Subtrair \n3 - Multiplicar \n4 - Dividir \n5 - Potência");
+	printf ("\nInforme sua opção: ");
+	scanf ("%d", &op);
+	return op;
+}
+
+main (void){
+	setlocale(LC_ALL, "Portuguese");
+	int op;
+
+	op = menu(op);
+	switch (op){
+		case 1:
+			soma();
+		break;
+
+		case 2:
+			subtracao();
+		break;
+		
+		case 3:
+			multiplicacao();	
+		break;
+		
+		case 4:
+			divisao();
+		break;
+		
+		case 5:
+			potencia();
+		break;	
+		
+		default:
+			printf ("Opção inválida!");
+		break;
+	}
 }
